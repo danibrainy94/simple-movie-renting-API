56495d87657e6a68bf477314a6513bd413e0c6b4
danibrainy94 <danibrainy94@gmail.com>
2024-01-28 23:19:38 +0100

    main

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
 
 // A simple Movie renting API
+class Movie {
+    constructor(title, genre, availableCopies) {
+        this.title = title;
+        this.genre = genre;
+        this.availableCopies = availableCopies;
+    }
+}
+
+
+class MovieStore {
+    constructor() {
+        this.movies = [
+        new Movie('Inception', 'Sci-Fi', 5),
+        new Movie('The Shawshank Redemption', 'Drama', 3),
+        new Movie('The Dark Knight', 'Action', 7),
+        ]
+        this.rentedMovies = []
+    }
+}
