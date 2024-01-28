88b19fce815e6ee801ef527cf16c4c114fa6c340
danibrainy94 <danibrainy94@gmail.com>
2024-01-28 23:20:29 +0100

    main

--- script.js
+++ script.js
@@ -18,4 +18,42 @@ class MovieStore {
         ]
         this.rentedMovies = []
     }
+
+    rentMovie(title) {
+        const movieIndex = this.movies.findIndex(movie => movie.title === title)
+
+
+        if (movieIndex !== -1 && this.movies[movieIndex].availableCopies > 0) {
+        this.movies[movieIndex].availableCopies--
+        this.rentedMovies.push(this.movies[movieIndex])
+        return `${title} rented successfully.`
+        } else {
+        return `Sorry, ${title} is not available for rent or no copies left.`
+        }
+    }
+
+
+    returnMovie(title) {
+        const rentedMovieIndex = this.rentedMovies.findIndex(movie => movie.title === title)
+
+
+        if (rentedMovieIndex !== -1) {
+        this.movies.push(this.rentedMovies[rentedMovieIndex])
+        this.rentedMovies.splice(rentedMovieIndex, 1)
+        return `${title} returned successfully.`
+        } else {
+        return `Error: ${title} was not rented from this store.`
+        }
+    }
+
+
+    displayAvailableMovies() {
+        return this.movies.map(movie => `${movie.title} (${movie.genre}) - Available Copies: ${movie.availableCopies}`).join('\n')
+    }
+
+
+    displayRentedMovies() {
+        return this.rentedMovies.map(movie => `${movie.title} (${movie.genre})`).join('\n')
+    }
 }
+
